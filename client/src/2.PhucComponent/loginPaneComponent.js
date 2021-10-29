7ce0e4908d035fef5358edd00a588c0d0c0c098b
hongphucvo <vohongphuc2001@gmail.com>
2021-10-30 00:16:12 +0700

    create component #TODO switch

---
+++ client/src/2.PhucComponent/loginPaneComponent.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import styled from 'styled-components';
+import { Container, Input, Row, Col,Button } from 'reactstrap';
+import Home from '../1.CatComponent/HomeComponent';
+/*   try = () => {
+        return (
+          <Router>
+            <Switch>
+              <Route exact path="/" component={App} />
+              <Route path="/user" component={User} />
+            </Switch>
+          </Router>
+        );
+      } */
+class LoginPane extends Component{
+    
+    render(){
+        const {phone,pwd}=this.props;   
+        
+        return(
+            <div classname="center">
+                <Row>
+                
+                <Col/><Col/>
+                <Col><div>
+            <h1>Đăng Nhập</h1>
+            Số điện thoại
+            <Input name="Phone" text={phone} type="number" required/>
+
+             Mật khẩu
+            <Input name="pwd" text={pwd} type="password" required/>
+            <Button className="exception">Quên mật khẩu?</Button>
+            
+            <Row align="center">
+            <Button onClick={this.try} color="primary" >Đăng nhập</Button>
+            <Route path='/home' component={Home}/>
+            </Row>
+            <Button className="exception">Chưa có tài khoản? Đăng ký</Button>
+            </div>
+            </Col>
+            <Col/>
+            </Row>
+            </div>
+        )
+    };
+    
+}
+export default LoginPane;
