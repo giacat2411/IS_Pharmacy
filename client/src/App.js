c607d3cf8bd6a170fc26b407fdfa9138d146e347
hongphucvo <vohongphuc2001@gmail.com>
2021-10-31 15:39:47 +0700

    fix appjs

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,15 @@ import './App.css';
 import { Component } from 'react';
 import Main from './1.CatComponent/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
-import Login from './2.PhucComponent/LoginComponent';
-import SignPwd from './2.PhucComponent/forgetpwd';
 class App extends Component {
   render() {
   return (
     <BrowserRouter>
     <div className="App">
-        <SignPwd />
+        <Main/>
     </div>
     </BrowserRouter>
   );
 }
 }
-
 export default App;
