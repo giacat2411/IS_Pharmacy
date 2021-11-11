638e6643b5a3ef16181ae70f31f9d92270bf3a71
DuyChanh <chanh.tran0901@hcmut.edu.vn>
2021-11-11 09:10:39 +0700

    Commit

---
+++ client/src/3.ChanhComponent/ManagerNur.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import DoctorSideBar from './DoctorSideBarComponent';
+
+class ViewSchedule extends Component {
+    render() {
+        return(
+            <div>
+                <DoctorSideBar />
+                <Container>
+                    <Row>
+                        <Col> Danh sách điều dưỡng </Col>
+                        <Col> Tìm thông tin điều dưỡng </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default ViewSchedule;
