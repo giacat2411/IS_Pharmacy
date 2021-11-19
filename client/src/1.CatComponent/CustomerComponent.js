43c98e1d65ab6f19bbdc70b0468e62b15ef5e3ec
Gia Cat <giacatnk@gmail.com>
2021-11-19 15:23:27 +0700

    a

--- client/src/1.CatComponent/CustomerComponent.js
+++ client/src/1.CatComponent/CustomerComponent.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+﻿import React, {Component} from 'react';
 import { NavLink } from 'reactstrap';
 import { Container, Row, Col } from 'reactstrap';
 import { Card, CardBody, CardTitle} from 'reactstrap';
@@ -9,7 +9,7 @@ class Customer extends Component{
         return(
             <Container>
                 <Row>
-                    <Col md="12"> <h1 className="cat-cushome-header">Chào mừng bạn đến với Health Care !</h1>
+                    <Col md="12"> <h1 className="cushome-header">Chào mừng bạn đến với Health Care !</h1>
                     </Col>
                 </Row>
                 <hr />
@@ -20,7 +20,7 @@ class Customer extends Component{
                         <Card className="cat-customer-item-right">
                             <img className="cat-customer-img" width="91.98px" height="90px" src="/assets/images/view-home.png" alt = "Xem hồ sơ bệnh án"></img>
                             <CardBody>
-                            <CardTitle tag="h5" className="cat-customer-text">Tra cứu</CardTitle>
+                            <CardTitle tag="h5" className="cat-text">Tra cứu</CardTitle>
                             </CardBody>
                         </Card>
                     </NavLink>
@@ -39,15 +39,47 @@ class Customer extends Component{
                         </LinkContainer>
                     </Col>
                     <Col md="6">
-                    <LinkContainer to ="/appointment" style={{cursor: 'pointer'}}>
+                        <LinkContainer to ="/cancelappointment" style={{cursor: 'pointer'}}>
                         <NavLink className="cat-customer-item-link">
-                            <Card className="cat-customer-item-right">
-                                <img className="cat-customer-img" width="67.29px" height="95px" src="/assets/images/make-treat-home.png" alt = "Đặt lịch khám"></img>
+                            <Card className="cat-customer-item-left">
+                                <img className="cat-customer-img" width="94px" height="95px" src="/assets/images/cancal-treat.png" alt = "Hủy lịch khám"></img>
                                 <CardBody>
-                                <CardTitle tag="h5" className="cat-customer-text">Đặt lịch khám</CardTitle>
+                                <CardTitle tag="h5" className="cat-customer-text">Hủy lịch khám</CardTitle>
                                 </CardBody>
                             </Card>
                             </NavLink>
+                        </LinkContainer>
+                    </Col>
+                </Row>
+<Container>
+                <Row>
+                    <Col md="12"> <h1 className="cushome-header">Chào mừng bạn đến với Health Care !</h1>
+                    </Col>
+                </Row>
+                <hr />
+                <Row>
+                    <Col md="6">
+                    <LinkContainer to ="/" style={{cursor: 'pointer'}}>
+                        <NavLink className="cat-customer-item-link">
+                        <Card className="cat-customer-item-right">
+                            <img className="cat-customer-img" width="91.98px" height="90px" src="/assets/images/view-home.png" alt = "Xem hồ sơ bệnh án"></img>
+                            <CardBody>
+                            <CardTitle tag="h5" className="cat-text">Tra cứu</CardTitle>
+                            </CardBody>
+                        </Card>
+                    </NavLink>
+                    </LinkContainer>    
+                    </Col>
+                    <Col md="6">    
+                    <LinkContainer to ="/buydrug" style={{cursor: 'pointer'}}>
+                        <NavLink className="cat-customer-item-link">
+                        <Card className="cat-customer-item-left">
+                            <img className="cat-customer-img" width="61.86px" height="85px" src="/assets/images/buy-home.png" alt = "Mua thuốc online"></img>
+                            <CardBody>
+                            <CardTitle tag="h5" className="cat-customer-text">Mua thuốc online</CardTitle>
+                            </CardBody>
+                        </Card>
+                        </NavLink>
                         </LinkContainer>
                     </Col>
                     <Col md="6">
@@ -63,6 +95,7 @@ class Customer extends Component{
                         </LinkContainer>
                     </Col>
                 </Row>
+            </Container>
             </Container>
         )
     }
